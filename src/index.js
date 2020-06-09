619151b78702482747c2de7ecf297e2892075c67
vasilieva-sasha <vasilieva.aleksandra@bk.ru>
2020-06-09 20:34:15 +0300

    создает компонент приветствия и рендерит на страницу

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {App} from "./components/app/app.jsx";
+import {Settings} from "./common/utils";
 
+ReactDOM.render(
+    <App errorsCount={Settings.ERRORS_COUNT}/>,
+    document.querySelector(`#root`)
+);
