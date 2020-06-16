1b8257e0938a4a3bb5835fcd55cc2b9e0d3bd13c
vasilieva-sasha <vasilieva.aleksandra@bk.ru>
2020-06-16 19:09:06 +0300

    добавляет новые компоненты экранов

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,15 +1,36 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import {WelcomeScreen} from "../welcome-screen/welcome-screen.jsx";
+import {QuestionArtistScreen} from "../question-artist-screen/question-artist-screen";
+import {QustionGenreScreen} from "../question-genre-screen/question-genre-screen";
 
 const welcomeButtonHandler = () => {};
 
 const App = (props) => {
   const {errorsCount} = props;
-  return <WelcomeScreen
-    errorsCount={errorsCount}
-    onWelcomeButtonClick={welcomeButtonHandler}
-  />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <WelcomeScreen
+            errorsCount={errorsCount}
+            onWelcomeButtonClick={welcomeButtonHandler}
+          />
+        </Route>
+        <Route exact path="/dev-artist">
+          <QuestionArtistScreen />
+        </Route>
+        <Route exact path="/dev-genre">
+          <QustionGenreScreen />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+  // return <WelcomeScreen
+  //   errorsCount={errorsCount}
+  //   onWelcomeButtonClick={welcomeButtonHandler}
+  // />;
 };
 
 App.propTypes = {
